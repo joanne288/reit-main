1af8f47b4d1a6ccb699e30df891b067ef6a25201
Joanne Mascarenhas <159116232+joanne288@users.noreply.github.com>
2026-04-27 23:05:41 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host:     process.env.DB_HOST     || 'localhost',
+  host:     process.env.DB_HOST     || 'mysql.railway.internal',
   user:     process.env.DB_USER     || 'root',
   password: process.env.DB_PASSWORD || 'golden',
   database: process.env.DB_NAME     || 'jmra_db',
